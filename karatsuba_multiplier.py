cac202d03a33e43b7988046d2c9920161a1d5ab9
s3nh <steam.panek@gmail.com>
2019-10-06 12:49:03 +0200

    Add stack and queues

--- karatsuba_multiplier.py
+++ karatsuba_multiplier.py
@@ -1,3 +1,4 @@
+import math
 from math import log10
 
 """
--- sorting/node_.py
+++ sorting/node_.py
@@ -13,5 +13,23 @@ def main():
     pass 
 
 
+class SinglyLinkedList:
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def append(self, data):
+        node = Node(data)
+        if self.head:
+            self.head.next = node
+            self.head = node
+        else:
+            self.tail = node
+            self.head = node
+
+
+
+
 if __name__ == "__main__":
     main()
